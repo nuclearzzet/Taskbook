8011a8ec35a25d10d5080fe3276aa92738023996
Niket John <niketjohn@outlook.com>
2022-05-28 00:23:05 +0530

    there are some migration desync and other issues

--- todo_app/main/models.py
+++ todo_app/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class NavLink(models.Model):
+    name =   models.CharField(verbose_name="name", max_length=50)
+
+
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return self.name
