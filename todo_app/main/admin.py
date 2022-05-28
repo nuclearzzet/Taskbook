e6f162463bb2a86ed1c4e7517558362e357890ff
Niket John <niketjohn@outlook.com>
2022-05-28 11:10:48 +0530

    database error fixed

--- todo_app/main/admin.py
+++ todo_app/main/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import(
-    NavLink
-)
-# Register your models here.
 
-admin.site.register(NavLink)
+# Register your models here.
--- todo_app/main/models.py
+++ todo_app/main/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class NavLink(models.Model):
-    name =   models.CharField(verbose_name="name", max_length=50)
-
-
-    def __str__(self):
-        return self.name
-    
-    def __repr__(self):
-        return self.name
